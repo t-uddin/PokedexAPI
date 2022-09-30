592acea595958184dd27bc5c4099555e6ed35833
Thamanna Uddin <57349131+t-uddin@users.noreply.github.com>
2022-09-30 17:05:15 +0100

    Add files via upload

---
+++ pokedex/src/main/java/com/pokedex/PokedexApplication.java
@@ -0,0 +1,18 @@
+package com.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexApplication {
+
+	public static void main(String[] args) {
+		cdjocerjrec
+		System.setProperty("javax.net.ssl.trustStore","clientTrustStore.key");
+		System.setProperty("javax.net.ssl.trustStorePassword","qwerty");
+
+
+		SpringApplication.run(PokedexApplication.class, args);
+	}
+
+}
